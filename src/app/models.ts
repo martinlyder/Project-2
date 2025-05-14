9af823c8493d224843fd73ff7e54a92d7e2c025b
Lyder <martinlyder@gmail.com>
2025-05-14 14:44:49 +0200

    Added llama 70b model

--- src/app/models.ts
+++ src/app/models.ts
@@ -10,9 +10,26 @@ export interface Model {
 }
 
 export const AVAILABLE_MODELS: Model[] = [
-   {
+  {
     id: "meta/meta-llama-3-8b-instruct",
     name: "An 8 billion parameter language model from Meta, fine tuned for chat",
+    parameters: {
+     top_k: 0,
+     top_p: 0.9,
+     max_tokens: 512,
+     min_tokens: 0,
+     temperature: 0.6,
+     system_prompt: "You are a helpful assistant",
+     length_penalty: 1,
+     stop_sequences: "<|end_of_text|>,<|eot_id|>",
+     prompt_template: "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\nYou are a helpful assistant<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n",
+     presence_penalty: 1.15,
+     log_performance_metrics: false
+    }
+  },
+  {
+    id: "meta/meta-llama-3-70b-instruct",
+    name: "A 70 billion parameter language model from Meta, fine tuned for chat completions",
     parameters: {
      top_k: 0,
      top_p: 0.9,
